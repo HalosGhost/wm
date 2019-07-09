cfe54be75d0ce1b1d6a27ec3da6b08275364cac9
halosghost <samuel.stuewe@gmail.com>
2019-07-08 22:01:47 -0500

    remove all mouse control; allow basic application launching

--- src/main.c
+++ src/main.c
@@ -8,45 +8,18 @@ main (void) {
 
     Window root = DefaultRootWindow(dpy);
 
-    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F1")), Mod1Mask,
+    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("d")), Mod1Mask,
             root, true, GrabModeAsync, GrabModeAsync);
-    XGrabButton(dpy, 1, Mod1Mask, root, true,
-            ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
-    XGrabButton(dpy, 3, Mod1Mask, root, true,
-            ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
 
     do {
-        static XWindowAttributes attr;
-        static XButtonEvent start = { .subwindow = None };
-
         static XEvent ev;
         XNextEvent(dpy, &ev);
 
         switch ( ev.type ) {
             case KeyPress:
-                if ( ev.xkey.subwindow != None ) {
-                    XRaiseWindow(dpy, ev.xkey.subwindow);
+                if ( fork() != -1 && setsid() != -1 ) {
+                    execv(*launcher, launcher);
                 } break;
-
-            case ButtonPress:
-                if ( ev.xbutton.subwindow != None ) {
-                    XGetWindowAttributes(dpy, ev.xbutton.subwindow, &attr);
-                    start = ev.xbutton;
-                } break;
-
-            case MotionNotify: {
-                int xdiff = ev.xbutton.x_root - start.x_root;
-                int ydiff = ev.xbutton.y_root - start.y_root;
-                XMoveResizeWindow(dpy, start.subwindow,
-                    attr.x + (start.button == 1 ? xdiff : 0),
-                    attr.y + (start.button == 1 ? ydiff : 0),
-                    (unsigned )max(1, attr.width + (start.button == 3 ? xdiff : 0)),
-                    (unsigned )max(1, attr.height + (start.button == 3 ? ydiff : 0)));
-            } break;
-
-            case ButtonRelease:
-                start.subwindow = None;
-                break;
         }
     } while (true);
 }
--- src/main.h
+++ src/main.h
@@ -1,6 +1,14 @@
 #include <X11/Xlib.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define max(a, b) (((a) > (b)) ? (a) : (b))
+
+static char * const launcher [] = {
+    "/usr/bin/rofi",
+    "-show",
+    "run",
+    NULL
+};
 
